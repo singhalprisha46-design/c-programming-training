fe853d3c85575ee0abffe604fbe8852adeb4ef5b
singhalprisha46-design <singhalprisha46@gmail.com>
2026-07-01 14:14:32 +0530

    Implement second largest element
    This program reads an array of integers and finds the second largest element. It also handles cases where there is no second largest element.

---
+++ Day-05/second_largest_element.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN)
+        cout << "No second largest element";
+    else
+        cout << "Second Largest: " << secondLargest;
+
+    return 0;
+}
