b83d00842926434270b6883a87bc58fc413bebef
singhalprisha46-design <singhalprisha46@gmail.com>
2026-07-01 14:57:27 +0530

    Add print_address.cpp to display value and address

---
+++ Day-07/print_address.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    cout << "Value: " << x << endl;
+    cout << "Address: " << &x << endl;
+
+    return 0;
+}
